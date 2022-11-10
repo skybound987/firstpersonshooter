60d201acb0072353eda95c81d8054da2b6a640b8
Jon Hendrix <skybound987@protonmail.com>
2022-11-09 23:11:19 -0500

    Main.py first draft, need to add notes

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("hello world")
+import pygame as pg
+import sys
+from settings import *
 
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+
+    def new_game(self):
+        pass
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K.ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
